1cc6c51924cae22316e0446b6e0c5b271aad6055
mamido62 <114516429+mamido62@users.noreply.github.com>
2022-10-01 12:03:46 +0300

    Update mamido.sh

--- mamido.sh
+++ mamido.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17071 --tls -u 1DXJKyTKmDN6jAtrfZGKbjs2sij4u9rXBU.Rig-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 8
+screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u Bgz3LqW5yusnaLSxzUdkVRmFqnAyYeSC9f.Rig-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 8
